1ca1d3322e2f174f54876f3cf625bf5e9cde3558
DenisSangi <denissangi84@gmail.com>
2024-09-24 13:27:10 +0500

    Update pom.xml 11 to 17 <maven.compiler.target>17</maven.compiler.target> <maven.compiler.source>17</maven.compiler.source>

--- src/main/java/user/RestAssuredClient.java
+++ src/main/java/user/RestAssuredClient.java
@@ -4,8 +4,6 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
-import static io.restassured.RestAssured.given;
-
 public class RestAssuredClient {
 
     protected static final String URL =   "https://stellarburgers.nomoreparties.site/api";
